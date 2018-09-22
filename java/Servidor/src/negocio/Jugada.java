1bd56057629cc8972b450d4340a4664a4634c48d
Amilcar Cattaneo <amilcar.cattaneo29@gmail.com>
2018-09-21 23:49:33 -0300

    Modifica la indentacion y agega nueva linea entre los metodos.

--- java/Servidor/src/negocio/Jugada.java
+++ java/Servidor/src/negocio/Jugada.java
@@ -1,26 +1,33 @@
 package negocio;
 
 public class Jugada {
-		private Jugador jugador;
-		private Carta carta;
-		private int orden;
-		public Jugador getJugador() {
-			return jugador;
-		}
-		public void setJugador(Jugador jugador) {
-			this.jugador = jugador;
-		}
-		public Carta getCarta() {
-			return carta;
-		}
-		public void setCarta(Carta carta) {
-			this.carta = carta;
-		}
-		public int getOrden() {
-			return orden;
-		}
-		public void setOrden(int orden) {
-			this.orden = orden;
-		}
-		
+
+	private Jugador jugador;
+	private Carta carta;
+	private int orden;
+
+
+	public Jugador getJugador() {
+		return jugador;
+	}
+
+	public void setJugador(Jugador jugador) {
+		this.jugador = jugador;
+	}
+
+	public Carta getCarta() {
+		return carta;
+	}
+
+	public void setCarta(Carta carta) {
+		this.carta = carta;
+	}
+
+	public int getOrden() {
+		return orden;
+	}
+
+	public void setOrden(int orden) {
+		this.orden = orden;
+	}
 }
