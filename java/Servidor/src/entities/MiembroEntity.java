85cdc1e04a16b5125df1677d63470fcbb3f5890f
Amilcar Cattaneo <amilcar.cattaneo29@gmail.com>
2018-10-10 21:34:58 -0300

    Agrega las clases de persistencia de Miembro.

---
+++ java/Servidor/src/entities/MiembroEntity.java
@@ -0,0 +1,72 @@
+package entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import negocio.Grupo;
+
+@Entity
+@Table(name = "Miembros")
+public class MiembroEntity {
+
+	@Id
+	private int idMiembro;
+	@OneToMany
+	@JoinColumn(name = "idMiembro")
+	private UsuarioEntity usuario;
+	@OneToMany
+	@JoinColumn(name = "idMiembro")
+	private GrupoEntity grupo;
+	private int puntaje;
+	private boolean enGrupo;
+
+	public MiembroEntity(int idMiembro, int puntaje, boolean enGrupo) {
+		super();
+		this.idMiembro = idMiembro;
+		this.puntaje = puntaje;
+		this.enGrupo = enGrupo;
+	}
+
+	public int getIdMiembro() {
+		return idMiembro;
+	}
+
+	public void setIdMiembro(int idMiembro) {
+		this.idMiembro = idMiembro;
+	}
+
+	public UsuarioEntity getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(UsuarioEntity usuario) {
+		this.usuario = usuario;
+	}
+
+	public GrupoEntity getGrupo() {
+		return grupo;
+	}
+
+	public void setGrupo(GrupoEntity grupo) {
+		this.grupo = grupo;
+	}
+
+	public int getPuntaje() {
+		return puntaje;
+	}
+
+	public void setPuntaje(int puntaje) {
+		this.puntaje = puntaje;
+	}
+
+	public boolean isEnGrupo() {
+		return enGrupo;
+	}
+
+	public void setEnGrupo(boolean enGrupo) {
+		this.enGrupo = enGrupo;
+	}
+}
