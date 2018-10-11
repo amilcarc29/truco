42cc33cce1a2ff8518c0feb825cdec3ce734514c
Amilcar Cattaneo <amilcar.cattaneo29@gmail.com>
2018-10-10 21:35:18 -0300

    Agrega los constructores.

--- java/Servidor/src/negocio/Experto.java
+++ java/Servidor/src/negocio/Experto.java
@@ -1,11 +1,12 @@
 package negocio;
 
-public class Experto  extends Categoria{
+public class Experto extends Categoria{
+
+	public Experto(int idCategoria, String nombre, int score, int minimoPartida, int minimoPuntos, int promedioMinimo) {
+		super(idCategoria, nombre, score, minimoPartida, minimoPuntos, promedioMinimo);
+	}
 
 	@Override
 	public void actualizar() {
-		// TODO Auto-generated method stub
-		
 	}
-
 }
--- java/Servidor/src/negocio/Master.java
+++ java/Servidor/src/negocio/Master.java
@@ -1,11 +1,12 @@
 package negocio;
 
-public class Master  extends Categoria{
+public class Master extends Categoria{
+
+	public Master(int idCategoria, String nombre, int score, int minimoPartida, int minimoPuntos, int promedioMinimo) {
+		super(idCategoria, nombre, score, minimoPartida, minimoPuntos, promedioMinimo);
+	}
 
 	@Override
 	public void actualizar() {
-		// TODO Auto-generated method stub
-		
 	}
-
 }
